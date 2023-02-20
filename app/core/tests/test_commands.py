47c1f6ca10c56e7b35feaba9919b59d515d9429c
Luka <lukalagvilava0@gmail.com>
2023-02-21 02:48:24 +0400

    Added a command to wait for the database to load

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,7 +11,7 @@ from django.test import SimpleTestCase
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTest(SimpleTestCase):
+class CommandTests(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
@@ -20,15 +20,15 @@ class CommandTest(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
-    
+        patched_check.assert_called_once_with(databases=['default'])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for databse when getting OperationalError"""
+        """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 +[True]
-        
+            [OperationalError] * 3 + [True]
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patch_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
